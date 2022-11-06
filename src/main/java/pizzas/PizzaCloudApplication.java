d0eb783d496c5536ecb1985f21bcae93b5fbe011
Ekaterina K <34368106+riarurainey@users.noreply.github.com>
2022-11-06 17:58:10 +0300

    part 1 - init

--- src/main/java/pizzas/PizzaCloudApplication.java
+++ src/main/java/pizzas/PizzaCloudApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PizzaCloudApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PizzaCloudApplication.class, args);
     }
-
 }
